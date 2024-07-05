9ee370fc1b1ed1a75e28c958e826246c05242427
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2024-07-05 22:45:33 +0330

    Add files via upload

--- src/functions.php
+++ src/functions.php
@@ -5,15 +5,15 @@ declare(strict_types = 1);
 namespace Tak;
 
 function async(callable $callback,mixed ...$args) : Task {
-	return (new Task)->async($callback,...$args);
+	return (new Tak\Async\Task)->async($callback,...$args);
 }
 
 function delay(float $seconds) : bool {
-	return (new Task)->sleep($seconds);
+	return (new Tak\Async\Task)->sleep($seconds);
 }
 
 function setErrorHandler(callable $callback = null) : void {
-	Errors::setErrorHandler($callback);
+	Tak\Async\Errors::setErrorHandler($callback);
 }
 
 ?>
